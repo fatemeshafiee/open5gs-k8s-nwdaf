c97addc56ca20673bdadcc679a4759a4c5c5cf3a
Fatemeh Shafiei <ardestanifateme0@gmail.com>
2025-01-12 19:33:51 -0500

    feat: one slice, 6 UEs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 kubectl apply -k mongodb -n open5gs
 kubectl apply -k open5gs -n open5gs
 sleep 60
+kubectl apply -k NWDAF -n open5gs
 kubectl apply -k ueransim/ueransim-gnb/ -n open5gs
 sleep 30
 kubectl apply -k ueransim/ueransim-ue -n open5gs
--- undeploy.sh
+++ undeploy.sh
@@ -2,3 +2,4 @@ kubectl delete -k ueransim/ueransim-ue -n open5gs
 kubectl delete -k ueransim/ueransim-gnb/ -n open5gs
 kubectl delete -k open5gs -n open5gs
 kubectl delete -k mongodb -n open5gs
+kubectl delete -k NWDAF -n open5gs
